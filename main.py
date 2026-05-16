e3928d209fa7751839c4e09f1beaa650afdcd14c
Evan Richards <evanrichards26@proton.me>
2026-05-16 15:03:09 +0100

    First commit.

---
+++ main.py
@@ -0,0 +1,36 @@
+from graph import *
+import markov_chain as mc
+
+va: Vertex = Vertex('a')
+vb: Vertex = Vertex('b')
+vc: Vertex = Vertex('c')
+
+dtmc1 = Graph(
+    {va, vb, vc},
+    {
+        Edge(va, vb, 1),
+        Edge(vb, vb, 2/3),
+        Edge(vb, vc, 1/3),
+        Edge(vc, vb, 1/2),
+        Edge(vc, va, 1/2)
+    }
+)
+
+dtmc2 = Graph(
+    {va, vb, vc},
+    {
+        Edge(va, vb, 1/2),
+        Edge(va, vc, 1/2)
+    }
+)
+
+dtmc3 = Graph(
+    {va, vb, vc},
+    {
+        Edge(va, vb, 1),
+        Edge(vb, vc, 1),
+        Edge(vc, va, 1)
+    }
+)
+
+print('->'.join([s.label for s in mc.run_dtmc(dtmc3, va, 18)]))
